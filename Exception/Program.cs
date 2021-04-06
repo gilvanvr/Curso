49a4c6c93e0f2142c5cd440e7adba69eaa7b8a2e
Gilvan Viana Rodrigues <gilvanvr@gmail.com>
2021-04-05 21:09:20 -0300

    [BL_DEV] Aula de exception.

---
+++ Exception/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Informe um número: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Informe outro número: ");
+                int b = int.Parse(Console.ReadLine());
+
+                int result = a / b;
+
+                Console.WriteLine("Resultado: " + result);
+            }
+            catch (System.DivideByZeroException e)
+            {
+                Console.WriteLine("Error! " + e.GetType());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format Error! " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Programa Finalizado!");
+            }
+
+        }
+    }
+}
