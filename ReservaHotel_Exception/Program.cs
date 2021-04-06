92b35226c9e7987a556c0ecb10fd4c16eab5d261
Gilvan Viana Rodrigues <gilvanvr@gmail.com>
2021-04-05 23:18:09 -0300

    [BL_DEV] Exercício de exception.

---
+++ ReservaHotel_Exception/Program.cs
@@ -0,0 +1,40 @@
+﻿using ReservaHotel_Exception.Entities;
+using ReservaHotel_Exception.Entities.Exceptions;
+using System;
+
+namespace ReservaHotel_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/mm/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/mm/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/mm/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/mm/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException ex)
+            {
+                Console.WriteLine("Error in reservation: " + ex.Message);
+            }
+        }
+    }
+}
